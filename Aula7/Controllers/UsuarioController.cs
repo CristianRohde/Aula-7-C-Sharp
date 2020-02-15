de0d8b162cb383960340897e7d4fa5b5bc1a1f4f
CristianRohde <cristianedis@gmail.com>
2020-02-15 14:11:42 -0300

    up MVC

---
+++ Aula7/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Aula7.Models; // temos que ter a referença 
+
+namespace Aula7.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private static UsuarioTeste _usuarios = new UsuarioTeste();
+        public IActionResult Index()
+        {
+           
+            return View(_usuarios.listaUsuario);
+        }
+        public IActionResult AdicionarUsuario() // Metodo Get
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarUsuario(UsuarioModel usuarioModel) // Metodo Post
+        {
+            _usuarios.CriaUsuario(usuarioModel);
+            return View();
+        }
+    }
+}
